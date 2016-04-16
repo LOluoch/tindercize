86382b27b536f45a867d96db0838cfbc7d08d4a7
Abdulrahman Osoble <abdulrahmanosoble@gmail.com>
2016-04-16 19:22:36 +0400

    /start functional

--- bot.js
+++ bot.js
@@ -5,19 +5,20 @@ var lastMessage = 1460810624;
 var token = "171177333:AAH1KcTa_ysvrqe3uuQrN7oJymhpwqbAIas";
 var Alice = new bot(token);
 
+var users = [];
+
 function getMessages() {
     Alice.getUpdates().then(function(rawData){
         data = JSON.parse(rawData);
         data.result.forEach(function(result,index){
-            if(result.message.date > lastMessage){
-                if(result.message.text == "/start")
+            msg = result.message;
+            id = results.from.id;
+            chat = result.chat.id;
+            if(msg.date > lastMessage){
+                if(msg.text == "/start")
                 {
-                    Alice.sendMessage(result.message.chat.id, "You have started.");
-                    console.log("You have started... sent to " + result.message.chat.first_name + " " + result.message.chat.last_name);
-                }
-                else{
-                    Alice.sendMessage(result.message.chat.id, "I don't know how to respond to that.");
-                    console.log("I dont know... sent to " + result.message.chat.first_name + " " + result.message.chat.last_name);
+                    users.push(id: {"phone": "0", "state": 0});
+                    Alice.sendMessage(chat, "Welcome to FitBot. Just tell me what kind of workout/exercises you want to do and I'll match you with someone who has similar interests!");
                 }
             }
         });
